1d9941918f6fa9c31e1d7824023a96a88b1be7e1
aesparza84 <aesparza@colum.edu>
2023-10-24 20:54:44 -0500

    Key notes

--- DataStructures/DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/DataStructures/Program.cs
@@ -11,7 +11,7 @@ namespace DataStructures
             ///     Better suited for LARGER data sets (efficiency when looking up)
             ///     Weak for smaller data sets. 
             ///     
-            ///Comment out to not create table.
+            ///Comment out to not create Hash Table.
             //HashTableExample newTable = new HashTableExample();
 
             ///The StackExample class demonstrates how a stack:
@@ -19,19 +19,72 @@ namespace DataStructures
             ///Go into class to see more.
             ///
             ///The stack property of LIFO (Last In First Out) is shown
-            ///Comment out to not create stack.
+            ///Comment out to not create Stack.
             //StackExample newStack = new StackExample();
 
             ///The QueueExample class demonstrates properties of the Queue.
-            ///This demonostrates the Queues's FIFO property of handling data.
+            ///This shows the Queues's FIFO property of handling data.
             ///Go into class to see more.
             ///
             ///Comment out to not create Queue
             //QueueExample newQueue = new QueueExample();
 
+            ///The ArrayExample class demonstrates the use of an array to store elements.
+            ///This shows how an array takes in data and how it iterates through its elements.
+            ///Go into class to see more.
+            ///
+            ///Comment out to not create Array
             //ArrayExample newArray = new ArrayExample(); 
 
 
+            //---Key notes about the data structures---//
+
+            ///HASH TABLES
+            ///
+            ///PRO: They better suited for larger data sets as inserting, lookup and delete
+            ///take on average O(1) to compute, worst case O(n).
+            ///
+            ///CON: When two keys lead to the same bucket (a collision) linked lists are used to search 
+            ///for the desired items, which is not ideal for large data sets.
+            ///
+
+            ///ARRAY
+            ///
+            ///PRO: Arrays are great for small data sets. Initializing an array keeps all the elements near
+            ///eachother in memory which allows for quick and efficient lookup. 
+            ///Insert takes O(1), lookup and delete takes O(n).
+            ///
+            ///CONS: The lookup taking O(n) makes arrays not ideal for large data sets, whereas Hash points you 
+            ///directly to the data location.
+            ///
+
+            ///STACK
+            ///
+            ///PRO: All operations take O(1) to perform.
+            ///
+            ///Stacks have a LIFO proerty (Last In First Out), where the LAST item to enter the stack
+            ///is the FIRST item to leave the stack. 
+            /// 
+            ///Stacks can be used for tracking data through a process. An example can be
+            ///the 'Undo' command in a lot of software. You can 'Pop' off the last command 
+            ///and return to the previous iteration.
+            ///
+            ///CONS: If you need to access something at the bottom of the stack, it would take O(n) time
+            ///and would be inefficient.
+            ///
+
+            ///QUEUE
+            ///
+            ///PRO: Very similar to Stacks, all operations take O(1) time to perform.
+            ///
+            ///Queues, unlike stacks, follow the FIFO method (First In First Out).
+            ///The First item to enter the list is the First item to be output.
+            ///
+            ///An example use could be handling web requests, where you would want to handle them in the order
+            ///that they were recieved.
+            ///
+            ///CONS: There is no random access, so searching for where an item is or if it contains an item
+            ///is not ideal with Queues as you would need to convert to an array.
             #region consecutive checking
             //int[] arrA = { 1, 2, 3, 4, 5, 6 };
             //int[] arrB = { 6,3,4,3,2,1};
@@ -57,9 +110,9 @@ namespace DataStructures
             //        isConsectuive = true;
             //        //if 1, go to next index, still consecutive
             //        //else, break out of loop
-                   
+
             //    }
-                
+
 
             //    if (isConsectuive)
             //    {
@@ -71,8 +124,8 @@ namespace DataStructures
             //        Console.WriteLine("Not COnsectuive");
             //        return false;
             //    }
-                
-                
+
+
             //}
 
             //int[] sortArray(int[] passedArray)
